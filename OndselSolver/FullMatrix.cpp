e5728858fefdec390ddda10b9b97f6adac9ed062
John Dupuy <jdupuy98@gmail.com>
2023-10-26 08:43:37 -0500

    rearranged some of the code

--- OndselSolver/FullMatrix.cpp
+++ OndselSolver/FullMatrix.cpp
@@ -37,39 +37,12 @@ inline void FullMatrix<double>::zeroSelf()
         this->at(i)->zeroSelf();
     }
 }
-template<>
-inline void FullMatrix<double>::identity() {
-    this->zeroSelf();
-    for (int i = 0; i < this->size(); i++) {
-        this->at(i)->at(i) = 1.0;
-    }
-}
 template<typename T>
 inline FColsptr<T> FullMatrix<T>::column(int j) {
     int n = (int)this->size();
     auto answer = std::make_shared<FullColumn<T>>(n);
     for (int i = 0; i < n; i++) {
         answer->at(i) = this->at(i)->at(j);
-    }
-    return answer;
-}
-template<typename T>
-inline FMatsptr<T> FullMatrix<T>::timesFullMatrix(FMatsptr<T> fullMat)
-{
-    int m = this->nrow();
-    auto answer = std::make_shared<FullMatrix<T>>(m);
-    for (int i = 0; i < m; i++) {
-        answer->at(i) = this->at(i)->timesFullMatrix(fullMat);
-    }
-    return answer;
-}
-template<typename T>
-inline FMatsptr<T> FullMatrix<T>::timesTransposeFullMatrix(FMatsptr<T> fullMat)
-{
-    int nrow = this->nrow();
-    auto answer = std::make_shared<FullMatrix<T>>(nrow);
-    for (int i = 0; i < nrow; i++) {
-        answer->at(i) = this->at(i)->timesTransposeFullMatrix(fullMat);
     }
     return answer;
 }
@@ -143,11 +116,6 @@ inline void FullMatrix<T>::symLowerWithUpper()
     }
 }
 template<typename T>
-inline void FullMatrix<T>::atiput(int i, FRowsptr<T> fullRow)
-{
-    this->at(i) = fullRow;
-}
-template<typename T>
 inline void FullMatrix<T>::atijput(int i, int j, T value)
 {
     this->at(i)->atiput(j, value);
@@ -176,27 +144,6 @@ inline void FullMatrix<T>::atijminusNumber(int i, int j, T value)
 {
     auto rowi = this->at(i);
     rowi->at(j) -= value;
-}
-template<>
-inline double FullMatrix<double>::sumOfSquares()
-{
-    double sum = 0.0;
-    for (int i = 0; i < this->size(); i++)
-    {
-        sum += this->at(i)->sumOfSquares();
-    }
-    return sum;
-}
-template<typename T>
-inline double FullMatrix<T>::sumOfSquares()
-{
-    assert(false);
-    return 0.0;
-}
-template<typename T>
-inline void FullMatrix<T>::zeroSelf()
-{
-    assert(false);
 }
 template<typename T>
 inline FMatsptr<T> FullMatrix<T>::copy()
@@ -233,77 +180,6 @@ inline void FullMatrix<T>::magnifySelf(T factor)
     }
 }
 template<typename T>
-inline std::ostream& FullMatrix<T>::printOn(std::ostream& s) const
-{
-    s << "FullMat[" << std::endl;
-    for (int i = 0; i < this->size(); i++)
-    {
-        s << *(this->at(i)) << std::endl;
-    }
-    s << "]";
-    return s;
-}
-template<typename T>
-inline std::shared_ptr<EulerParameters<T>> FullMatrix<T>::asEulerParameters()
-{
-    //"Given [A], compute Euler parameter."
-
-    auto traceA = this->trace();
-    T dum = 0.0;
-    T dumSq = 0.0;
-    //auto qE = CREATE<EulerParameters<double>>::With(4); //Cannot use CREATE.h in subclasses of std::vector. Why?
-    auto qE = std::make_shared<EulerParameters<T>>(4);
-    qE->initialize();
-    auto OneMinusTraceDivFour = (1.0 - traceA) / 4.0;
-    for (int i = 0; i < 3; i++)
-    {
-        dumSq = this->at(i)->at(i) / 2.0 + OneMinusTraceDivFour;
-        dum = (dumSq > 0.0) ? std::sqrt(dumSq) : 0.0;
-        qE->atiput(i, dum);
-    }
-    dumSq = (1.0 + traceA) / 4.0;
-    dum = (dumSq > 0.0) ? std::sqrt(dumSq) : 0.0;
-    qE->atiput(3, dum);
-    T max = 0.0;
-    int maxE = -1;
-    for (int i = 0; i < 4; i++)
-    {
-        auto num = qE->at(i);
-        if (max < num) {
-            max = num;
-            maxE = i;
-        }
-    }
-
-    if (maxE == 0) {
-        auto FourE = 4.0 * qE->at(0);
-        qE->atiput(1, (this->at(0)->at(1) + this->at(1)->at(0)) / FourE);
-        qE->atiput(2, (this->at(0)->at(2) + this->at(2)->at(0)) / FourE);
-        qE->atiput(3, (this->at(2)->at(1) - this->at(1)->at(2)) / FourE);
-    }
-    else if (maxE == 1) {
-        auto FourE = 4.0 * qE->at(1);
-        qE->atiput(0, (this->at(0)->at(1) + this->at(1)->at(0)) / FourE);
-        qE->atiput(2, (this->at(1)->at(2) + this->at(2)->at(1)) / FourE);
-        qE->atiput(3, (this->at(0)->at(2) - this->at(2)->at(0)) / FourE);
-    }
-    else if (maxE == 2) {
-        auto FourE = 4.0 * qE->at(2);
-        qE->atiput(0, (this->at(0)->at(2) + this->at(2)->at(0)) / FourE);
-        qE->atiput(1, (this->at(1)->at(2) + this->at(2)->at(1)) / FourE);
-        qE->atiput(3, (this->at(1)->at(0) - this->at(0)->at(1)) / FourE);
-    }
-    else if (maxE == 3) {
-        auto FourE = 4.0 * qE->at(3);
-        qE->atiput(0, (this->at(2)->at(1) - this->at(1)->at(2)) / FourE);
-        qE->atiput(1, (this->at(0)->at(2) - this->at(2)->at(0)) / FourE);
-        qE->atiput(2, (this->at(1)->at(0) - this->at(0)->at(1)) / FourE);
-    }
-    qE->conditionSelf();
-    qE->calc();
-    return qE;
-}
-template<typename T>
 inline T FullMatrix<T>::trace()
 {
     T trace = 0.0;
@@ -312,54 +188,6 @@ inline T FullMatrix<T>::trace()
         trace += this->at(i)->at(i);
     }
     return trace;
-}
-template<typename T>
-inline double FullMatrix<T>::maxMagnitude()
-{
-    double max = 0.0;
-    for (int i = 0; i < this->size(); i++)
-    {
-        double element = this->at(i)->maxMagnitude();
-        if (max < element) max = element;
-    }
-    return max;
-}
-template<typename T>
-inline FColsptr<T> FullMatrix<T>::bryantAngles()
-{
-    auto answer = std::make_shared<FullColumn<T>>(3);
-    auto sthe1y = this->at(0)->at(2);
-    T the0x, the1y, the2z, cthe0x, sthe0x, y, x;
-    if (std::abs(sthe1y) > 0.9999) {
-        if (sthe1y > 0.0) {
-            the0x = std::atan2(this->at(1)->at(0), this->at(1)->at(1));
-            the1y = M_PI / 2.0;
-            the2z = 0.0;
-        }
-        else {
-            the0x = std::atan2(this->at(2)->at(1), this->at(2)->at(0));
-            the1y = M_PI / -2.0;
-            the2z = 0.0;
-        }
-    }
-    else {
-        the0x = std::atan2(-this->at(1)->at(2), this->at(2)->at(2));
-        cthe0x = std::cos(the0x);
-        sthe0x = std::sin(the0x);
-        y = sthe1y;
-        if (std::abs(cthe0x) > std::abs(sthe0x)) {
-            x = this->at(2)->at(2) / cthe0x;
-        }
-        else {
-            x = this->at(1)->at(2) / -sthe0x;
-        }
-        the1y = std::atan2(y, x);
-        the2z = std::atan2(-this->at(0)->at(1), this->at(0)->at(0));
-    }
-    answer->atiput(0, the0x);
-    answer->atiput(1, the1y);
-    answer->atiput(2, the2z);
-    return answer;
 }
 template<typename T>
 inline bool FullMatrix<T>::isDiagonal()
