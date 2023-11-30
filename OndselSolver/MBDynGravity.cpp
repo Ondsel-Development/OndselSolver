308cb186a2540dd103bd0b4c9b58e56d9c29881b
John Dupuy <john@cattailcreek9.com>
2023-11-30 12:04:41 -0600

    linker fixes for mac

--- OndselSolver/MBDynGravity.cpp
+++ OndselSolver/MBDynGravity.cpp
@@ -4,55 +4,56 @@
 #include "ASMTConstantGravity.h"
 #include "ASMTAssembly.h"
 
-using namespace MbD;
+namespace MbD {
+    void MBDynGravity::parseMBDyn(std::string line)
+    {
+        gravityString = line;
+        size_t previousPos = 0;
+        auto pos = line.find(":");
+        auto front = line.substr(previousPos, pos - previousPos);
+        assert(front.find("gravity") != std::string::npos);
+        auto arguments = std::vector<std::string>();
+        std::string argument;
+        while (true) {
+            previousPos = pos;
+            pos = line.find(",", pos + 1);
+            if (pos != std::string::npos) {
+                argument = line.substr(previousPos + 1, pos - previousPos - 1);
+                arguments.push_back(argument);
+            }
+            else {
+                argument = line.substr(previousPos + 1);
+                arguments.push_back(argument);
+                break;
+            }
+        }
+        assert(arguments.at(0).find("uniform") != std::string::npos);
+        arguments.erase(arguments.begin());
+        gvec = readPosition(arguments);
+        assert(arguments.at(0).find("string") != std::string::npos);
+        arguments.erase(arguments.begin());
+        auto iss = std::istringstream(arguments.at(0));
+        iss >> formula;
+        formula = std::regex_replace(formula, std::regex("\""), "");
+        double mag;
+        iss = std::istringstream(formula);
+        iss >> mag;
 
-void MbD::MBDynGravity::parseMBDyn(std::string line)
-{
-	gravityString = line;
-	size_t previousPos = 0;
-	auto pos = line.find(":");
-	auto front = line.substr(previousPos, pos - previousPos);
-	assert(front.find("gravity") != std::string::npos);
-	auto arguments = std::vector<std::string>();
-	std::string argument;
-	while (true) {
-		previousPos = pos;
-		pos = line.find(",", pos + 1);
-		if (pos != std::string::npos) {
-			argument = line.substr(previousPos + 1, pos - previousPos - 1);
-			arguments.push_back(argument);
-		}
-		else {
-			argument = line.substr(previousPos + 1);
-			arguments.push_back(argument);
-			break;
-		}
-	}
-	assert(arguments.at(0).find("uniform") != std::string::npos);
-	arguments.erase(arguments.begin());
-	gvec = readPosition(arguments);
-	assert(arguments.at(0).find("string") != std::string::npos);
-	arguments.erase(arguments.begin());
-	auto iss = std::istringstream(arguments.at(0));
-	iss >> formula;
-	formula = std::regex_replace(formula, std::regex("\""), "");
-	double mag;
-	iss = std::istringstream(formula);
-	iss >> mag;
+        arguments.erase(arguments.begin());
+        gvec->normalizeSelf();
+        gvec->magnifySelf(mag);
+    }
 
-	arguments.erase(arguments.begin());
-	gvec->normalizeSelf();
-	gvec->magnifySelf(mag);
+    void MBDynGravity::readFunction(std::vector<std::string>& args)
+    {
+        assert(false);
+    }
+
+    void MBDynGravity::createASMT()
+    {
+        auto asmtGravity = std::make_shared<ASMTConstantGravity>();
+        asmtGravity->setg(gvec);
+        asmtAssembly()->setConstantGravity(asmtGravity);
+    }
 }
 
-void MbD::MBDynGravity::readFunction(std::vector<std::string>& args)
-{
-	assert(false);
-}
-
-void MbD::MBDynGravity::createASMT()
-{
-	auto asmtGravity = std::make_shared<ASMTConstantGravity>();
-	asmtGravity->setg(gvec);
-	asmtAssembly()->setConstantGravity(asmtGravity);
-}
